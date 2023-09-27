fb833011daddab39523116473737d794d26aa9e6
Luis Felipe da Costa Janes <luisf.janes@gmail.com>
2023-09-27 18:19:55 -0300

    feat: initing array and add value

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
-﻿namespace consoleArray
+﻿using System.Runtime.InteropServices;
+
+namespace consoleArray
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.Clear();
+            var myArr = new int[5];
+            for (int i = 0; i < myArr.Length; i++)
+            {
+                myArr[i] = i;
+                Console.WriteLine(myArr[i]);
+            }
+            foreach (int value in myArr)
+            {
+                Console.WriteLine(value);
+            }
         }
     }
 }
